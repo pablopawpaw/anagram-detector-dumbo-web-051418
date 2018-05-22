edab2d4079f68243b8f9c33f9cfb7103685cab42
pablopawpaw <lpault29@gmail.com>
2018-05-22 15:14:20 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize word
+    @word = word
+  end
+
+  def match array
+    # binding.pry
+    array.map do |word|
+      if word.downcase.split("").sort == self.word.downcase.split("").sort
+        word
+      else
+        nil
+      end
+    end.compact
+  end
+end
